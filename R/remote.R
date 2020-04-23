2141987df659c14d577daeb88175a901a0ce1462
Noam Ross (the Robot Version) <ross@ecohealthalliance.org>
2020-04-23 10:40:02 -0400

    Add first functions for remote connection

---
+++ R/remote.R
@@ -0,0 +1,37 @@
+#' Connect to the remote REPEL database
+#' 
+#' Returns a connection to the remote REPEL database, hosted on a Postgres server.
+#' `REPELDATA_USER` and `REPELDATA_PASSWORD` should be set in your environment
+#' (e.g. `.Renviron` file) to provide access to the data.
+#' 
+#' @param host The host URL.  Defaults to the EHA server
+#' @param port the host connection port.  Generally does not need to be set
+#' @param user User login name.  In general to be set in `.Renviron`
+#' @param password User login passwword. In general to be set in `.Renviron`
+#'
+#' @return a DBI connection object
+#' @export
+#'
+repel_remote_conn <- function(host = NULL, port = NULL, user = NULL, password = NULL) {
+    conn <- DBI::dbConnect(
+        RPostgres::Postgres(),
+        host  =host %||%  Sys.getenv("REPEL_REMOTE_HOST", "localhost"),
+        port = port %||% Sys.getenv("REPEL_REMOTE_PORT", 22053),
+        user = user %||% Sys.getenv("REPELDATA_USER"),
+        password = password %||% Sys.getenv("REPELDATA_PASSWORD"),
+        dbname = Sys.getenv("repel")
+    )
+    return(conn)
+}
+
+#' Show REPEL (remote) database in the RStudio Connections Pane 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+repel_remote_pane <- function() {
+    wahis_pane(conn = repel_remote_conn(), dbname = "REPEL remote database",
+               connectCode = "wahisclient::repel_remote_pane()")
+}
+
