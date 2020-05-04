6bc3ffe41c3022fa3e95f9b1a9c1348d06350c44
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-05-04 14:40:31 -0400

    brackets

--- R/schema.R
+++ R/schema.R
@@ -2,4 +2,6 @@
 #' Database documention is available here: https://github.com/ecohealthalliance/repel-docs/blob/master/database-schema.md
 #' @return a tibble object
 #' @export
-repel_schema <- function() wahis::repel_schema()
+repel_schema <- function(){
+    wahis::repel_schema()
+}
